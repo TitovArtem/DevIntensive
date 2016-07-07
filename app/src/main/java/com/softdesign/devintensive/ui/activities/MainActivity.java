0186c5b1234526d775991e7951d6eaa05fba1e1d
artem_bars <pbi62007@yandex.ru>
2016-07-07 22:11:39 +0300

    Add rounded avatar on the navigation view (only for square pics) and close navigation view by pressing android back button.

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -13,10 +15,12 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.data.managers.DataManager;
 import com.softdesign.devintensive.utils.ConstantManager;
+import com.softdesign.devintensive.utils.RoundedAvatarDrawable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +33,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     /** Edit mode. It's 1 if data is modifying and 0 in other case. */
     private int mCurrentEditMode = 0;
 
+    private ImageView mUserAvatar;
+    private NavigationView mNavigationView;
     private CoordinatorLayout mCoordinatorLayout;
     private Toolbar mToolbar;
     private DrawerLayout mNavigationDrawer;
@@ -46,11 +52,15 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
         mDataManager = DataManager.getInstance();
 
-
         mCoordinatorLayout = (CoordinatorLayout)findViewById(R.id.main_coordinator_container);
         mToolbar = (Toolbar)findViewById(R.id.toolbar);
         mNavigationDrawer = (DrawerLayout)findViewById(R.id.navigation_drawer);
+        mNavigationView = (NavigationView)findViewById(R.id.navigation_view);
         mFab = (FloatingActionButton)findViewById(R.id.fab);
+
+        View headerLayout = mNavigationView.getHeaderView(0);
+        mUserAvatar = (ImageView)headerLayout.findViewById(R.id.drawer_header_avatar);
+
         mUserPhone = (EditText)findViewById(R.id.phone_et);
         mUserMail = (EditText)findViewById(R.id.mail_et);
         mUserVk = (EditText)findViewById(R.id.vk_et);
@@ -69,6 +79,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
         setupToolbar();
         setupDrawer();
+        roundAvatar();
         loadUserInfoValue();
 
 
@@ -140,6 +151,26 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         outState.putInt(ConstantManager.EDIT_MODE_KEY, mCurrentEditMode);
     }
 
+
+    @Override
+    public void onBackPressed() {
+
+        if (mNavigationDrawer.isDrawerOpen(GravityCompat.START)) {
+            mNavigationDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /** Makes avatar on NavigationView like rounded. */
+    protected void roundAvatar() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inMutable = false;
+        Bitmap avatar = BitmapFactory.decodeResource(getResources(), R.drawable.user_photo, options);
+        RoundedAvatarDrawable roundedAvatarDrawable = new RoundedAvatarDrawable(avatar);
+        mUserAvatar.setImageDrawable(roundedAvatarDrawable);
+    }
+
     private void showSnackbar(String message) {
         Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
     }
@@ -154,8 +185,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     }
 
     private void setupDrawer() {
-        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(
+        mNavigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem item) {
